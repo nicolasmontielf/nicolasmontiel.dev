d6282ceb6bb5fdfde6b519d2260c93fd5c29ee4c
nicolasmontielf <nicolasmontielf@gmail.com>
2026-04-11 19:57:59 -0300

    fix: update vercel import to use the correct module

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,11 +2,11 @@ import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
 import preact from "@astrojs/preact";
 
-import vercel from "@astrojs/vercel/serverless";
+import vercel from "@astrojs/vercel";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), preact()],
-  output: 'server',
-  adapter: vercel()
+    integrations: [tailwind(), preact()],
+    output: 'server',
+    adapter: vercel()
 });
