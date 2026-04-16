6f48d8a9d79fc83ef771a0e29c64c86f3b92a541
nicolasmontielf <nicolasmontielf@gmail.com>
2026-04-16 15:23:00 -0300

    fix: add site URL to Astro configuration

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,7 @@ import vercel from "@astrojs/vercel";
 
 // https://astro.build/config
 export default defineConfig({
+    site: 'https://www.nicolasmontiel.dev',
     integrations: [tailwind(), preact()],
     output: 'server',
     adapter: vercel()
